ba00eb211519144e9b133e2f28eb168bb57abcb2
drezos <andreasmallia28@gmail.com>
2025-12-12 10:14:23 +0100

    Delete frontend/src/hooks directory

--- frontend/src/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-// Export all hooks from a single entry point
-export { useMetrics, useMetricsHistory, useMetricsSummary } from './useMetrics';
-export { useCompetitors, useCompetitor } from './useCompetitors';
-export { useAlerts, useAlertCount } from './useAlerts';
